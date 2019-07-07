70f1d81604e8e958b8868a6877ee76486b05e873
Tomer du Sautoy <tomerdusautoy@gmail.com>
2019-07-07 13:09:03 -0500

    get and getId implemented

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// implement your API here
+const express = require('express');
+const db = require('./data/db');
+
+const server = express();
+
+server.get('/', (req, res) => {
+  res.send('hello world');
+})
+
+server.get('/api/users', (req, res) => {
+  db.find()
+    .then(users => {
+      res.status(201).json(users);
+    })
+    .catch(err => {
+      sendUserError(500, 'Data not found', res)
+      return;
+    })
+});
+
+server.get('/api/users/:id', (req, res) => {
+  const id = req.params.id;
+  db.findById(id)
+    .then(user => {
+      res.status(201).json(user);
+    })
+    .catch(err => {
+      sendUserError(500, 'Data not found', res)
+      return;
+    })
+})
+
+// server.post('/api/users', (req, res) => {
+
+// })
+
+server.listen(8000, () => console.log('API running on port 8000'));
